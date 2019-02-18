673ff64fe3850d04c47bcdd7f1b4cd59d1f80ffd
Marta Sarkane <sarkane.marta@gmail.com>
2019-02-18 21:11:47 +0200

    D1 continued

--- D1/src/d1/D1.java
+++ D1/src/d1/D1.java
@@ -15,11 +15,17 @@ public class D1 {
      */
     public static void main(String[] args) {
         
-        //int, char, bool, double vai flow, string
-        vardaIevade();                      
+        //datu tips int, char, bool, double vai flow, string
+        
+//        vardaIevade();          
+//        sazarosanas();
+//        ievade();
+        loginsUnParole();
+        
+        
     }
     
-    private static void vardaIevade () 
+    private static void vardaIevade() 
     {
         System.out.println("Ievadi savu vārdu:"); // shortcut: sout un tad tab
         
@@ -33,5 +39,110 @@ public class D1 {
         
         System.out.println("Tavs vārds ir " + vards + " " + uzvards); 
     }
+    
+    private static void sazarosanas()
+    {   
+        int a = 6;
+        if(a==5)
+        {
+            
+        }
+        
+        
+        // lai  pārbaudītu stringus
+        String b = "b";
+        String g = "g";
+        
+        if (b.equals("a")) {
+        }
+        else if (b.equals(g)) {
+        }
+        else {
+        }
+        
+        switch(a){
+            case 1:
+                //kas notiek
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+    }
+    
+    private static void ievade()
+    {
+        System.out.println("Ievadi skaitli");
+        
+        Scanner sc = new Scanner (System.in); 
+        
+        int skaitlis =  sc.nextInt(); 
+        
+        if (skaitlis >= 5)
+        {
+            System.out.println("**");                
+        }
+        else
+        {
+             System.out.println("*");   
+        }
+
+        String simboli = "aaa";
+        int a = simboli.length(); // 3
+    }
+    
+    private static void loginsUnParole() {
+        
+        String logins = "lietotajs";
+        String parole = "abc123";
+        
+        //3-13 logins
+        //5-10 parole
+                
+        // vai sakrit
+        
+        System.out.println("Ievadi savu lietotājvārdu:");
+        
+        Scanner sc = new Scanner (System.in);
+            
+        String loginsIevade =  sc.nextLine(); 
+        int loginsSK = loginsIevade.length();
+        
+        
+        if (loginsSK < 3 || loginsSK > 13 )
+        {
+            System.out.println("Lietotājvārds ir nepareiza garuma");                
+        }
+        else if (loginsIevade.equals(logins))
+        {
+             System.out.println("Lietotājvārds ir pareizs");   
+        }
+        else
+        {
+             System.out.println("Lietotājvārds nesakrīt");   
+        }
+        
+        
+        System.out.println("Ievadi savu paroli:"); 
+        
+        String paroleIevade = sc.nextLine();   
+        int paroleSK = paroleIevade.length();
+        
+        
+        if (paroleSK < 3 || paroleSK > 13 )
+        {
+            System.out.println("Parole ir nepareiza garuma");                
+        }
+        else if (paroleIevade.equals(parole))
+        {
+             System.out.println("Parole ir pareiza");   
+        }
+        else
+        {
+             System.out.println("Parole nesakrīt");   
+        }
+    }   
+    
     
 }
